4038304399294469b867ec376d3729525c8087ef
Akrosh <metiwariakrosh02@gmail.com>
2026-03-02 18:19:23 +0545

    feat: connection on mongoDB and create diff data model

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const path = require('path');
 const { logger } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
-const PORT = process.env.PORT || 2771;
 const cookieParser = require('cookie-parser')
-const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const cors = require('cors')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+const { logEvents } = require('./middleware/logger')
+const PORT = process.env.PORT || 2771;
 
+console.log(process.env.NODE_ENV)
 
+connectDB()
 app.use(logger)
 app.use(cors(corsOptions))
 app.use(express.json())
@@ -30,4 +36,13 @@ app.use((req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once('open', () => {
+    console.log('Connected to mongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
